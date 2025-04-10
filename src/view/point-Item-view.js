fce3ad63117637e4a440e6f4f1b615e185fabf2a
Xenia <k.nikolaevna98@gmail.com>
2025-04-10 20:14:40 +0300

    Исправления ESLint

--- src/view/point-Item-view.js
+++ src/view/point-Item-view.js
@@ -1,4 +1,4 @@
-import AbstractView from "../framework/view/abstract-view";
+import AbstractView from '../framework/view/abstract-view';
 
 import {
   getFormattedEventDay,
@@ -6,9 +6,9 @@ import {
   getFormattedTimeEvent,
   getFormattedAttrDatatimeEvent,
   getTimeDuration,
-} from "../utils";
+} from '../utils';
 
-import { typeIcons } from "/src/const.js";
+import { typeIcons } from '/src/const.js';
 
 function createLayout(pointData, destinationsData, offersData) {
   const {
@@ -57,8 +57,8 @@ function createLayout(pointData, destinationsData, offersData) {
             <time class="event__date" datetime="${eventDayAttr}">${eventDay}</time>
             <div class="event__type">
               <img class="event__type-icon" width="42" height="42" src="${
-                typeIcons[type]
-              }" alt="Event type icon">
+  typeIcons[type]
+}" alt="Event type icon">
             </div>
             <h3 class="event__title">${nameOfdestination}</h3>
             <div class="event__schedule">
@@ -75,20 +75,20 @@ function createLayout(pointData, destinationsData, offersData) {
             <h4 class="visually-hidden">Offers:</h4>
             <ul class="event__selected-offers">
             ${selectedOffers
-              .map(
-                (item) => `
+    .map(
+      (item) => `
               <li class="event__offer">
                 <span class="event__offer-title">${item.title}</span>
                 &plus;&euro;&nbsp;
                 <span class="event__offer-price">${item.price}</span>
               </li>
             `
-              )
-              .join("")}
+    )
+    .join('')}
             </ul>
             <button class="event__favorite-btn ${
-              isFavorite ? "event__favorite-btn--active" : ""
-            } " type="button">
+  isFavorite ? 'event__favorite-btn--active' : ''
+} " type="button">
               <span class="visually-hidden">Add to favorite</span>
               <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
                 <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -119,8 +119,8 @@ export default class PointItemView extends AbstractView {
   }
 
   #setEventListeners() {
-    const itemElement = this.element.querySelector(".event__rollup-btn");
-    itemElement.addEventListener("click", this.#handleClick);
+    const itemElement = this.element.querySelector('.event__rollup-btn');
+    itemElement.addEventListener('click', this.#handleClick);
   }
 
   get template() {
